f1fbe094dabdc04dc03f5fa929260ce9e3522d0d
Jared Mandap <jaredmandap@gmail.com>
2022-12-28 15:13:52 -0600

    add test quotes/data

--- src/pages/QuoteAll.js
+++ src/pages/QuoteAll.js
@@ -1,5 +1,12 @@
+import QuoteList from '../components/quotes/QuoteList'
+
+const TEST_QUOTES = [
+    { id: 'q1', author: 'Brandon Sanderson', text: 'My life to yours. My breath become yours.' },
+    { id: 'q2', author: 'Kaladin Stormblessed', text: "Honor is dead. But i'll see what I can do." },
+];
+
 const QuoteAll = () => {
-    return <h1>All Quotes Page</h1>
+    return <QuoteList quotes={TEST_QUOTES} />
 };
 
 export default QuoteAll;
